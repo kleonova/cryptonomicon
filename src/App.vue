4a7b105195e98b1386ee0ff5175ede810e304b6a
lev <kleonova0@gmail.com>
2021-08-03 00:56:34 +0800

    install tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-screen h-full bg-gray-100">
+  <div class="w-screen min-h-screen bg-gray-100">
     <div class="container mx-auto flex flex-col items-center p-4">
       <div class="container">
         <!--	?			-->
@@ -21,6 +21,7 @@
                   class="
                     block
                     w-full
+                    p-2
                     pr-10
                     border-gray-300
                     text-gray-900
@@ -125,18 +126,7 @@
           </div>
 
           <button @click="selectedTicket = null" type="button" class="absolute top-0 right-0">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              xmlns:xlink="http://www.w3.org/1999/xlink"
-              version="1.1"
-              width="30"
-              height="30"
-              x="0"
-              y="0"
-              viewBox="0 0 511.76 511.76"
-              style="enable-background: new 0 0 512 512"
-              xml:space="preserve"
-            >
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" x="0" y="0" viewBox="0 0 511.76 511.76" xml:space="preserve">
               <g>
                 <path
                   d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
@@ -220,4 +210,4 @@ export default {
 }
 </script>
 
-<style src="./assets/styles/app.css"></style>
+<style></style>
